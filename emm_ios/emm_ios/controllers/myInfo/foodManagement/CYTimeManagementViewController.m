bcb58a93b9817a1d10b96d654ae3d0889a3b14e6
bangzi <674862292@qq.com>
2017-10-21 10:05:15 +0800

    优化程序

--- emm_ios/emm_ios/controllers/myInfo/foodManagement/CYTimeManagementViewController.m
+++ emm_ios/emm_ios/controllers/myInfo/foodManagement/CYTimeManagementViewController.m
@@ -8,8 +8,13 @@
 
 #import "CYTimeManagementViewController.h"
 
-@interface CYTimeManagementViewController ()
+@interface CYTimeManagementViewController ()<UIPickerViewDelegate,UIPickerViewDataSource>
 
+@property (strong, nonatomic) IBOutlet UIPickerView *startTimePickerView;
+@property (strong, nonatomic) IBOutlet UIPickerView *endTimePickerView;
+
+@property (nonatomic ,strong)NSArray *hoursArr;
+@property (nonatomic ,strong)NSArray *mintiusArr;
 
 @end
 
@@ -19,25 +24,74 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title =@"设置营业时间";
-    
-
-    
+    //设置代理
+    self.startTimePickerView.delegate = self;
+    self.startTimePickerView.dataSource = self;
+//    self.endTimePickerView.delegate = self;
+//    self.endTimePickerView.dataSource = self;
     
 }
+#pragma  mark --懒加载
+- (NSArray *)hoursArr{
+    if (!_hoursArr) {
+         _hoursArr = @[@"00",@"01"] ;
+    }
+    return _hoursArr;
+}
+
+-(NSArray *)mintiusArr{
+    if (!_mintiusArr) {
+        _mintiusArr = @[@"00",@"02"];
+    }
+    return _mintiusArr;
+}
+
+#pragma  mark -- UIPickerViewDelegate UiPickerViewDataSource
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 2;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    NSLog(@"%lu",(unsigned long)self.hoursArr.count);
+    if (component == 0) {
+        
+        return self.hoursArr.count;
+        
+    } else {
+        
+        return self.mintiusArr.count;
+    }
+}
+
+- ( NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+  
+    if (component == 0) {
+        
+        return self.hoursArr[row];
+        
+    } else {
+        
+        return self.mintiusArr[row];
+    }
+}
+//- (nullable NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component{
+//    return nil;
+//}
+//- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(nullable UIView *)view {
+//    return nil;
+//}
+
+//- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+//   
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
